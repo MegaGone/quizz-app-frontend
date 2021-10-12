0f5819d8d9c11d1eac8df0200a6f4d94ea3dd382
J.Martinez <jmartinez@develsystems.com>
2021-10-11 19:43:41 -0700

    Table works!

---
+++ src/app/interfaces/index.ts
@@ -0,0 +1,3 @@
+export * from  "./login.interface";
+export * from  "./quizlist.interface";
+export * from  "./register-user.interface";
---
+++ src/app/interfaces/quizlist.interface.ts
@@ -0,0 +1,6 @@
+export interface Quiz {
+    id: string | number,
+    name: string,
+    code: string,
+    participants: number
+}
