b865b80a5c6b214372ad9f318136997a2342d78a
Javier Martínez - Junior Software Engineer <jmartinez@develsystems.com>
2022-04-05 21:06:40 -0600

    Profile UI v0.1

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/app/interfaces';
+import { AuthService } from 'src/app/services';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styles: [
-  ]
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public User!: IUser;
+
+  constructor(private authSvc: AuthService) { }
 
   ngOnInit(): void {
+    this.getUserDetails();
   }
 
+  getUserDetails() {
+    this.authSvc.getSession().subscribe(res => {
+      if(res != undefined) {
+        this.User = res;
+        console.log(this.User);
+      }
+    })
+  }
 }
