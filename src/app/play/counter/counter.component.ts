9e2ff4321293207a656ba68106f70c63372af854
Javier Martínez <jmartinez@develsystems.com>
2022-06-26 12:48:28 -0600

    Start to work in the counter component

--- src/app/play/counter/counter.component.ts
+++ src/app/play/counter/counter.component.ts
@@ -8,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
 
-  constructor() { }
+  public counter: number;
+  public setInterval!: ReturnType<typeof setTimeout>;
 
-  ngOnInit(): void {
+  constructor() { 
+    this.counter = 6;
   }
 
+  ngOnInit(): void {
+    this.initCounter();
+  }
+
+  /**
+   * INIT THE COUNTER
+   */
+  initCounter() {
+    this.setInterval = setInterval(() => {
+      
+      this.counter -= 1
+      if (this.counter === 0) {
+        clearInterval(this.setInterval)
+      }
+
+    }, 1000)
+  }
 }
